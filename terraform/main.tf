5f6860d54c5115fc087605d5157c82e3967dcda4
Ganesh <ganeshprajapati903@gmail.com>
2026-04-21 13:50:50 +0530

    Completed Kubernetes, monitoring, and ArgoCD setup

---
+++ terraform/main.tf
@@ -0,0 +1,129 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.10.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "${var.project_name}-sg"
+  description = "Allow SSH, HTTP and app port"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_key_pair" "ec2_key" {
+  key_name   = "devops-platform-key"
+  public_key = file("${path.module}/devops-platform-key.pub")
+}
+
+resource "aws_ecr_repository" "app_repo" {
+  name = "${var.project_name}-repo"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-repo"
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.ec2_key.key_name
+
+  tags = {
+    Name = "${var.project_name}-ec2"
+  }
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  value = aws_vpc.main_vpc.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.ec2_sg.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.app_server.public_ip
+}
+
+output "ecr_repo_url" {
+  value = aws_ecr_repository.app_repo.repository_url
+}
